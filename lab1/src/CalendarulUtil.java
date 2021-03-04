d843a1ff466a05df5fd2e342c7b7928e3bce3883
tatumarian <tatumarian99@gmail.com>
2021-03-04 14:13:52 +0200

    Clasa noua

--- lab1/src/CalendarulUtil.java
+++ lab1/src/CalendarulUtil.java
@@ -1,5 +1,10 @@
 
 public class CalendarulUtil {
-	
+	public String weekDay3(int day) {
+		String [] days= {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+		if(day<=0||day>7)
+			return null;
+		return days[day-1];
+	}
 
 }
