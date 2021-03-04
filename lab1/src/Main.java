c7b28ab4242aecf20fd91857366d93580e067557
tatumarian <tatumarian99@gmail.com>
2021-03-04 14:18:05 +0200

    print in main

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		CalendarulUtil cu=new CalendarulUtil();
+		System.out.println(cu.weekDay3(3));
 	}
 
 }
